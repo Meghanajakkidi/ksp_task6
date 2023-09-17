efe69245f5095626ce8f0c40ccab9aecf488af4d
Meghanajakkidi <144362663+Meghanajakkidi@users.noreply.github.com>
2023-09-17 11:04:23 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,49 @@
+print("hello world")
+#string methods#
+firstname = "harry"
+lastname ="porter"
+print(firstname)
+print(lastname)
+print(type(firstname))
+print(lastname[4])
+print(len(firstname))
+#for loop#
+for a in range(15):
+    print(a)
+#if statement#
+a = 25
+b = 33
+if a<b :
+    print("a is small")
+#if else#
+number = 44
+if number>46 :
+    print("number is true") 
+else:
+    print("number is false")  
+#elif#
+time = 12
+if time<12 :
+    print("good morning") 
+elif time>10 :
+    print("good afternoon")   
+else:
+    print("good night")  
+ #functions#
+def sample1():
+    print("hello guys")
+    print("welcome to the webpage")
+sample1()
+def demo(name):#parameter#
+    print("hello" + name)
+demo("world")
+def demo1(name1,name2):
+    print(name1+name2)
+demo1(200,300)
+ 
+
+
+
+
+    
+
